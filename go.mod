e03f881bb12ad4ae64467baf3822bb8fad29ed17
Neur0toxine <pashok9825@gmail.com>
2026-06-23 21:59:24 +0300

    fix: ws listener

--- go.mod
+++ go.mod
@@ -1,20 +1,20 @@
 module bot-quick-replies
 
-go 1.25
+go 1.25.0
 
 require (
+	github.com/gorilla/websocket v1.5.3
 	github.com/joho/godotenv v1.4.0
 	github.com/retailcrm/api-client-go v1.3.8
-	github.com/retailcrm/bot-api-client-go v0.0.13
+	github.com/retailcrm/bot-api-client-go v0.0.19
 )
 
 require (
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/h2non/gock v1.2.0 // indirect
 	github.com/lerenn/asyncapi-codegen v0.45.3 // indirect
 	github.com/oapi-codegen/runtime v1.1.2 // indirect
-	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/time v0.15.0 // indirect
 )
--- websocket.go
+++ websocket.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"regexp"
 
+	"github.com/gorilla/websocket"
 	mgbot "github.com/retailcrm/bot-api-client-go"
 	mgbotws "github.com/retailcrm/bot-api-client-go/ws"
 )
 
 type WebsocketListener struct {
-	ctx         context.Context
-	ws          *mgbotws.AppController
+	wsEndpoint  string
+	token       string
 	mg          mgbot.ClientInterface
 	scope       string
 	trigger     string
@@ -43,13 +49,11 @@ func NewWebsocketListener(endpoint, token, trigger, scope string, textOptions []
 		log.Fatal(err)
 	}
 
-	ws, err := mgbotws.NewController(regexp.MustCompile(`^http(s)?\:`).ReplaceAllString(uri, "ws$1:")+"ws", token)
-	if err != nil {
-		log.Fatal(err)
-	}
+	wsEndpoint := regexp.MustCompile(`^http(s)?\:`).ReplaceAllString(uri, "ws$1:") + "ws"
 
 	return &WebsocketListener{
-		ws:          ws,
+		wsEndpoint:  wsEndpoint,
+		token:       token,
 		mg:          mg,
 		scope:       scope,
 		trigger:     trigger,
@@ -58,46 +62,104 @@ func NewWebsocketListener(endpoint, token, trigger, scope string, textOptions []
 }
 
 func (l *WebsocketListener) Listen(ctx context.Context) error {
-	log.Println("Listening for the new messages...")
+	log.Println("Listening for the new messages:", l.wsEndpoint)
 
-	return l.ws.SubscribeToReceiveEventsOperation(ctx, mgbotws.EventsChannelParameters{
-		Events: "message_new",
-	}, func(ctx context.Context, event mgbotws.EventMessageFromEventsChannel) error {
-		wh, ok := event.Payload.Data.(mgbotws.MessageDataSchema)
-		if !ok {
-			return nil
-		}
+	conn, err := l.connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
 
-		if wh.Message.From != nil && wh.Message.From.Type != mgbotws.UserTypeCustomer {
-			return nil
-		}
-
-		if l.trigger != "" && wh.Message.Content != nil && *wh.Message.Content != l.trigger {
-			return nil
-		}
-
-		if wh.Message.From != nil {
-			log.Printf("Received message from %s with id=%d\n", wh.Message.From.Name, wh.Message.Id)
-		} else {
-			log.Printf("Received message with id=%d\n", wh.Message.Id)
-		}
-
-		_, err := l.mg.SendMessage(context.Background(), mgbot.SendMessageJSONRequestBody{
-			Type:           ptr(mgbot.MessageTypeText),
-			Content:        ptr("The quick brown fox jumps over the lazy dog."),
-			Scope:          mgbot.MessageScope(l.scope),
-			ChatID:         wh.Message.ChatId,
-			QuoteMessageID: wh.Message.Id,
-			TransportAttachments: &mgbot.MessageTransportAttachments{
-				Suggestions: l.suggestions,
-			},
-		})
+	for {
+		_, message, err := conn.ReadMessage()
 		if err != nil {
-			log.Printf("error: cannot respond to the message: %s\n", err)
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				return err
+			}
 		}
 
+		var event mgbotws.EventMessageFromEventsChannel
+		if err := json.Unmarshal(message, &event.Payload); err != nil {
+			log.Printf("error: cannot parse websocket event: %s\n", err)
+			continue
+		}
+
+		if err := l.handleEvent(ctx, event); err != nil {
+			log.Printf("error: cannot handle websocket event: %s\n", err)
+		}
+	}
+}
+
+func (l *WebsocketListener) connect(ctx context.Context) (*websocket.Conn, error) {
+	wsURL, err := url.Parse(l.wsEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	query := wsURL.Query()
+	query.Set("events", string(mgbotws.EventTypeMessageNew))
+	wsURL.RawQuery = query.Encode()
+
+	headers := http.Header{
+		"X-Bot-Token": []string{l.token},
+	}
+
+	conn, resp, err := websocket.DefaultDialer.DialContext(ctx, wsURL.String(), headers)
+	if err == nil {
+		return conn, nil
+	}
+
+	if resp == nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return nil, fmt.Errorf("%w: websocket handshake failed with HTTP %d; additionally failed to read response body: %v", err, resp.StatusCode, readErr)
+	}
+
+	return nil, fmt.Errorf("%w: websocket handshake failed with HTTP %d: %s", err, resp.StatusCode, string(body))
+}
+
+func (l *WebsocketListener) handleEvent(ctx context.Context, event mgbotws.EventMessageFromEventsChannel) error {
+	wh, ok := event.Payload.Data.(mgbotws.MessageDataSchema)
+	if !ok {
 		return nil
+	}
+
+	if wh.Message.From != nil && wh.Message.From.Type != mgbotws.UserTypeCustomer {
+		return nil
+	}
+
+	if l.trigger != "" && wh.Message.Content != nil && *wh.Message.Content != l.trigger {
+		return nil
+	}
+
+	if wh.Message.From != nil {
+		log.Printf("Received message from %s with id=%d\n", wh.Message.From.Name, wh.Message.Id)
+	} else {
+		log.Printf("Received message with id=%d\n", wh.Message.Id)
+	}
+
+	_, err := l.mg.SendMessage(ctx, mgbot.SendMessageJSONRequestBody{
+		Type:           ptr(mgbot.MessageTypeText),
+		Content:        ptr("The quick brown fox jumps over the lazy dog."),
+		Scope:          mgbot.MessageScope(l.scope),
+		ChatID:         wh.Message.ChatId,
+		QuoteMessageID: wh.Message.Id,
+		TransportAttachments: &mgbot.MessageTransportAttachments{
+			Suggestions: l.suggestions,
+		},
 	})
+	if err != nil {
+		return fmt.Errorf("cannot respond to the message: %w", err)
+	}
+
+	return nil
 }
 
 func ptr[T any](v T) *T {
